db0f964ff5a1ae852fcadd29f17655ab16fbf380
Phillipe <phillipe@galaxy.lan>
2026-06-07 09:49:28 -0400

    started wiring up the DUT in the testbench.

--- sim/tb.cpp
+++ sim/tb.cpp
@@ -20,6 +20,9 @@
  */
 
  #include "verilated.h"
+ #include "lib_axis_agent.h"
+
+ static constexpr unsigned BYTES = 4;
 
 
 // -----------------------------------------------------------------------------
@@ -38,6 +41,39 @@
  * @return 0 on success; aborts via assert on failure.
  */
 int main(int argc, char** argv) {
-    VerilatedContext ctx;
-    ctx.commandArgs(argc, argv);
+  VerilatedContext ctx;
+
+  ctx.commandArgs(argc, argv);
+
+  // DUT
+  auto* dut = new Vaxis_passthrough(&ctx);
+
+  // Stimulus interfaces to DUT
+  axis_if<BYTES> m_axis;
+  axis_if<BYTES> s_axis;
+
+  //---------------------------------
+  // Wire interfaces to DUT
+  //---------------------------------
+  // AXIS into DUT
+  m_axis.tvalid = &dut->s_tvalid_i;
+  m_axis.tdata  = &dut->s_tdata_i ;
+  m_axis.tkeep  = &dut->s_tkeep_i ;
+  m_axis.tlast  = &dut->s_tlast_i ;
+  m_axis.tready = &dut->s_tready_o;
+
+  // AXIS out of DUT
+  s_axis.tvalid = &dut->m_tvalid_o;
+  s_axis.tdata  = &dut->m_tdata_o ;
+  s_axis.tkeep  = &dut->m_tkeep_o ;
+  s_axis.tlast  = &dut->m_tlast_o ;
+  s_axis.tready = &dut->m_tready_i;
+  dut->m_tready_i = 1;
+
+  //---------------------------------
+  // Instantiate agent components
+  //---------------------------------
+  axis_master_driver<BYTES> driver(m_axis);
+  axis_monitor<BYTES> in_mon(m_axis);
+  axis_monitor<BYTES> out_mon(s_axis);
 }
